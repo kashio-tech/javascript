2ea4e80533d7dc7e5ef296c1e1e5c36dd11c56a0
kashio-tech <gumgum.yk.29@gmail.com>
2019-11-20 19:25:29 +0900

    [start]6-6

--- jquery/app.js
+++ jquery/app.js
@@ -1,3 +1,26 @@
-$(document).ready(function(){
-	$("body").html("<h1>Hello jQuery!!</h1>");
-});
+// $(document).ready(function () {
+//     $('body').html('<h1>Hello jQuery!!</h1>');
+// });
+// ###javascript 5章
+
+// $(function(){
+// 	// $('.box1').slideUp();  //slideDown
+// 	// $('.box1').show();
+// 	// $('.box1').css({'background-color': '#0000FF'});
+// 	$('.box1').hide();
+
+// 	});
+
+
+// ##以下、確認問題
+
+
+    // $(function(){
+    //     $('.box1').slideDown(function() {
+    //         $('.box1').css({
+    //             'background-color': '#0000FF',
+    //             'width': '200px',
+    //             'height': '100px'
+    //         }).slideUp();
+    //     });
+    // });
